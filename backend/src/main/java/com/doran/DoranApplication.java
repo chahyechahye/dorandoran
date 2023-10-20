b9bbebdccc64aa316e244058929b21e045623eb4
한성현 <hsh5349@kakao.com>
2023-10-20 17:46:11 +0900

    init

--- backend/src/main/java/com/doran/DoranApplication.java
+++ backend/src/main/java/com/doran/DoranApplication.java
@@ -1,13 +1,22 @@
 package com.doran;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class DoranApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DoranApplication.class, args);
+	}
+
+	@Bean
+	JPAQueryFactory jpaQueryFactory(EntityManager entityManager) {
+		return new JPAQueryFactory(entityManager);
 	}
 
 }
