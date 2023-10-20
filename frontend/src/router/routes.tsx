64913bceadb1ff0ab408c416f5779b722d0077be
Ssuhyeong <tngud124@gmail.com>
2023-10-20 10:35:09 +0900

    [S09P31B108] feat : front 개발환경 세팅

---
+++ frontend/src/router/routes.tsx
@@ -0,0 +1,53 @@
+import React, { FC } from "react";
+import { Route, Routes } from "react-router-dom";
+
+type DashboardLayout = any;
+
+interface RouteType {
+  path: string;
+  layout?: FC<DashboardLayout>;
+  element: React.LazyExoticComponent<FC>;
+}
+
+const routes: RouteType[] = [
+  {
+    path: "/",
+    element: React.lazy(() => import("@/pages/main")),
+  },
+  // {
+  //   path: "/",
+  //   element: () => <Navigate replace to="/main" />,
+  // },
+  // {
+  //   path: "/*",
+  //   element: React.lazy(() => import("@/pages/404")),
+  // },
+];
+
+const RenderRoutes = () => {
+  return (
+    <React.Suspense fallback={<div>loading...</div>}>
+      <Routes>
+        {routes.map((route, i) => {
+          const RouteElement = route.element;
+          const RouteLayout = route.layout || React.Fragment;
+          //   const Guard = route.guard || React.Fragment;
+
+          return (
+            <Route
+              key={i}
+              path={route.path}
+              element={
+                <RouteLayout>
+                  <RouteElement />
+                </RouteLayout>
+              }
+            />
+          );
+        })}
+      </Routes>
+    </React.Suspense>
+  );
+};
+
+export default RenderRoutes;
