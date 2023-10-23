bcb1c5f943c818faffc710c10e0f34c2f5ef78da
한성현 <hsh5349@kakao.com>
2023-10-23 15:25:47 +0900

    [S09P31B108-213] test: test

--- backend/src/main/java/com/doran/DoranApplication.java
+++ backend/src/main/java/com/doran/DoranApplication.java
@@ -1,15 +1,17 @@
 package com.doran;
 
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import jakarta.persistence.EntityManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import jakarta.persistence.EntityManager;
+
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class DoranApplication {
-
+	//푸시테스트
 	public static void main(String[] args) {
 		SpringApplication.run(DoranApplication.class, args);
 	}
