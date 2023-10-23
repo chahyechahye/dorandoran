ef441f7f443746c8f1c0ac812b51ff393adb0a53
한성현 <hsh5349@kakao.com>
2023-10-23 15:55:50 +0900

    [S09P31B108-213] feat: 시큐리티를 위한 UserDetails 오버라이딩 진행

--- backend/src/main/java/com/doran/user/entity/User.java
+++ backend/src/main/java/com/doran/user/entity/User.java
@@ -1,5 +1,10 @@
 package com.doran.user.entity;
 
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import com.doran.user.type.Roles;
 import com.doran.utils.sens.BaseTimeEntity;
 
@@ -18,7 +23,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class User extends BaseTimeEntity {
+public class User extends BaseTimeEntity implements UserDetails {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "user_id")
@@ -26,4 +31,40 @@ public class User extends BaseTimeEntity {
 
 	private Roles userRole;
 
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		return null;
+	}
+
+	//getUserName은 계정 고유한 값을 리턴하는 역할을 함
+	//도란도란은 편의상 pk를 반환하는 형식으로 진행
+	@Override
+	public String getUsername() {
+		return String.valueOf(this.id);
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return false;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return false;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return false;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return false;
+	}
 }
