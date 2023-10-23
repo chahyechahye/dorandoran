d572aa94e16767e5df0ad93b2ffb37ca3cfbcf12
한성현 <hsh5349@kakao.com>
2023-10-23 14:30:23 +0900

    [S09P31B108-209] fix: 경로 변경

--- backend/src/main/java/com/doran/exception/controller/ExceptionController.java
+++ backend/src/main/java/com/doran/exception/controller/ExceptionController.java
@@ -1,8 +1,5 @@
 package com.doran.exception.controller;
 
-
-import com.cocopay.exception.dto.CustomException;
-import com.cocopay.exception.dto.ErrorResponseEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
@@ -11,30 +8,31 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
+import com.doran.exception.dto.CustomException;
+import com.doran.exception.dto.ErrorResponseEntity;
+
 @RestControllerAdvice
 public class ExceptionController {
 
-    @ExceptionHandler(CustomException.class)
-    public ResponseEntity<ErrorResponseEntity> ExceptionHandler(CustomException e) {
-        return ErrorResponseEntity.toResponseEntity(e.getErrorCode());
-    }
+	@ExceptionHandler(CustomException.class)
+	public ResponseEntity<ErrorResponseEntity> ExceptionHandler(CustomException e) {
+		return ErrorResponseEntity.toResponseEntity(e.getErrorCode());
+	}
 
-    //모든 예외를 처리할 메소드
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<?> exceptionHandler(Exception e) {
-        e.printStackTrace();
+	//모든 예외를 처리할 메소드
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<?> exceptionHandler(Exception e) {
+		e.printStackTrace();
 
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("에러");
-    }
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("에러");
+	}
 
-
-    //404 (NoHandlerFoundException)
-    @ExceptionHandler(NoHandlerFoundException.class)
-    @ResponseStatus(value = HttpStatus.NOT_FOUND)
-    public ResponseEntity<?> noHandlerFoundExceptionHandler(Exception e, Model model) {
-        e.printStackTrace();
-        return ResponseEntity.status(404).body(e.getMessage());
-    }
-
+	//404 (NoHandlerFoundException)
+	@ExceptionHandler(NoHandlerFoundException.class)
+	@ResponseStatus(value = HttpStatus.NOT_FOUND)
+	public ResponseEntity<?> noHandlerFoundExceptionHandler(Exception e, Model model) {
+		e.printStackTrace();
+		return ResponseEntity.status(404).body(e.getMessage());
+	}
 
 }
