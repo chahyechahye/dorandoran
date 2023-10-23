49ca480bce49bf9642a6df555c2202c8d7dd2b51
HongJiMin <baker0929@naver.com>
2023-10-23 17:26:26 +0900

    [S09P31B108-202] feat : 버킷 init

---
+++ backend/src/main/java/com/doran/book/service/BookService.java
@@ -0,0 +1,10 @@
+package com.doran.book.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class BookService {
+}
---
+++ backend/src/main/java/com/doran/utils/bucket/dto/InsertDto.java
@@ -0,0 +1,18 @@
+package com.doran.utils.bucket.dto;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class InsertDto {
+    private MultipartFile file;
+
+    private String name;
+}
