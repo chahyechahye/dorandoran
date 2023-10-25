b539c320b92a0a368409625548c60940309350f1
황준혁 <hyug0819@gmail.com>
2023-10-25 22:43:38 +0900

    [S09P31B108-270] feat : Animal Dto 작성

---
+++ backend/src/main/java/com/doran/animal/dto/res/AnimalListDto.java
@@ -0,0 +1,14 @@
+package com.doran.animal.dto.res;
+
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AnimalListDto {
+
+	private List<AnimalDto> animalList;
+
+}
