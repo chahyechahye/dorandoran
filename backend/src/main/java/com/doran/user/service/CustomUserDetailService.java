e4de527d125b310c0fda258fe2afa1ae93b5deda
한성현 <hsh5349@kakao.com>
2023-10-26 12:46:43 +0900

    [S09P31B108-229] refactor : 객체 변경

--- backend/src/main/java/com/doran/user/service/CustomUserDetailService.java
+++ backend/src/main/java/com/doran/user/service/CustomUserDetailService.java
@@ -4,9 +4,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import com.doran.parent.type.Provider;
-import com.doran.user.dto.req.UserTokenBaseDto;
 import com.doran.user.type.Roles;
+import com.doran.utils.common.UserInfo;
 
 import io.jsonwebtoken.Claims;
 import lombok.extern.slf4j.Slf4j;
@@ -17,25 +16,18 @@ public class CustomUserDetailService {
 
 	public UserDetails loadUserByUsername(Claims userInfo) throws UsernameNotFoundException {
 		Object userRole = userInfo.get("userRole");
-		Object provider = userInfo.get("provider");
-		Provider provider1 = Provider.valueOf(provider.toString());
 		Roles roles = Roles.valueOf(userRole.toString());
 
 		int userId = (int)userInfo.get("userId");
 		int parentId = (int)userInfo.get("parentId");
-		// int childId = (int)userInfo.get("childId");
 		// int selectProfileId = (int)userInfo.get("selectProfileId");
 		log.info("userId : {}", userId);
 		log.info("parentId : {}", parentId);
-		// log.info("childId : {}", childId);
 		// log.info("selectProfileId : {}", selectProfileId);
 
-		UserTokenBaseDto build = UserTokenBaseDto.builder()
+		UserInfo build = UserInfo.builder()
 			.userId(userId)
-			// .childId(childId)
-			.parentId(parentId)
-			// .selectProfileId(selectProfileId)
-			.provider(provider1)
+			.selectProfileId(null)
 			.userRole(roles)
 			.build();
 		log.info(build.toString());
