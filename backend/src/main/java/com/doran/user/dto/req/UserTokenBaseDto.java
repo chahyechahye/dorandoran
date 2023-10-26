d83031a304b02e5d65eb25e565553bb6e8c89978
한성현 <hsh5349@kakao.com>
2023-10-26 12:27:47 +0900

    [S09P31B108-229] feat : 커스텀디테일서비스 직접 구현

--- backend/src/main/java/com/doran/user/dto/req/UserTokenBaseDto.java
+++ backend/src/main/java/com/doran/user/dto/req/UserTokenBaseDto.java
@@ -1,18 +1,65 @@
 package com.doran.user.dto.req;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import com.doran.parent.type.Provider;
 import com.doran.user.type.Roles;
 
+import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
-public class UserTokenBaseDto {
+@Data
+@Builder
+public class UserTokenBaseDto implements UserDetails {
 	private int userId;
 	private Integer parentId;
 	private Integer childId;
 	private Integer selectProfileId;
 	private Provider provider;
 	private Roles userRole;
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		List<GrantedAuthority> authorities = new ArrayList<>();
+		authorities.add(new SimpleGrantedAuthority(this.userRole.getRole()));
+		return authorities;
+	}
+
+	@Override
+	public String getPassword() {
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+		return null;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return false;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return false;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return false;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return false;
+	}
 }
