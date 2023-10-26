d62425260dff8186d40e1a9e0c4f8b884eae4c99
황준혁 <hyug0819@gmail.com>
2023-10-26 11:18:34 +0900

    [S09P31B108-270] feat : Animal 단일 조회 구현

--- backend/src/main/java/com/doran/animal/repository/AnimalRepositoryCustom.java
+++ backend/src/main/java/com/doran/animal/repository/AnimalRepositoryCustom.java
@@ -3,8 +3,11 @@ package com.doran.animal.repository;
 import java.util.Optional;
 
 import com.doran.animal.dto.res.AnimalListDto;
+import com.doran.animal.entity.Animal;
 
 public interface AnimalRepositoryCustom {
+
+	Optional<Animal> selectAnimal(int id);
 
 	Optional<AnimalListDto> selectAllAnimal();
 
--- backend/src/main/java/com/doran/animal/repository/AnimalRepositoryCustomImpl.java
+++ backend/src/main/java/com/doran/animal/repository/AnimalRepositoryCustomImpl.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 
 import com.doran.animal.dto.res.AnimalDto;
 import com.doran.animal.dto.res.AnimalListDto;
+import com.doran.animal.entity.Animal;
 import com.doran.animal.mapper.AnimalMapper;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -19,10 +20,19 @@ public class AnimalRepositoryCustomImpl implements AnimalRepositoryCustom {
 	private final AnimalMapper animalMapper;
 
 	@Override
+	public Optional<Animal> selectAnimal(int id) {
+		return Optional.ofNullable(
+			jpaQueryFactory.select(animal)
+						   .from(animal)
+						   .where(animal.id.eq(id))
+						   .fetchOne());
+	}
+
+	@Override
 	public Optional<AnimalListDto> selectAllAnimal() {
 		List<AnimalDto> animalList = jpaQueryFactory.select(Projections.fields(
 														AnimalDto.class,
-														animal.id.as("animalId"),
+														animal.id,
 														animal.name,
 														animal.imgUrl)).from(animal)
 													.fetch();
