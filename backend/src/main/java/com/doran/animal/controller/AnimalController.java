d23541495a8d03d0b4f99b0f54cf9959bb69ea07
황준혁 <hyug0819@gmail.com>
2023-10-26 11:18:45 +0900

    [S09P31B108-270] feat : Animal Controller 기능 추가

--- backend/src/main/java/com/doran/animal/controller/AnimalController.java
+++ backend/src/main/java/com/doran/animal/controller/AnimalController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.doran.animal.dto.res.AnimalListDto;
+import com.doran.animal.service.AnimalService;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -15,9 +18,11 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/api/animal")
 public class AnimalController {
 
+	private final AnimalService animalService;
+
 	@GetMapping("/all")
-	public ResponseEntity<?> animalList() {
-		return ResponseEntity.ok().build();
+	public ResponseEntity<AnimalListDto> animalList() {
+		return ResponseEntity.ok(animalService.selectAllAnimal());
 	}
 
 	@PostMapping("")
