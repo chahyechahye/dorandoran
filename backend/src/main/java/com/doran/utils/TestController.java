e13e1f32cca13b9789c7817ad3eb4e4120e233d1
한성현 <hsh5349@kakao.com>
2023-10-26 17:06:25 +0900

    [S09P31B108-243] test : 초대코드 테스터 생성

--- backend/src/main/java/com/doran/utils/TestController.java
+++ backend/src/main/java/com/doran/utils/TestController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.doran.animal.dto.res.AnimalDto;
 import com.doran.animal.service.AnimalService;
+import com.doran.redis.invite.key.Invite;
+import com.doran.redis.invite.service.InviteService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,10 +19,21 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RequestMapping("/api/test")
 public class TestController {
-	private final AnimalService animalService;
+    private final AnimalService animalService;
+    private final InviteService inviteService;
 
-	@GetMapping("/animal/{id}")
-	public ResponseEntity<AnimalDto> selectAnimal(@PathVariable int id) {
-		return ResponseEntity.ok(animalService.selectAnimal(id));
-	}
+    @GetMapping("/animal/{id}")
+    public ResponseEntity<AnimalDto> selectAnimal(@PathVariable int id) {
+        return ResponseEntity.ok(animalService.selectAnimal(id));
+    }
+
+    @GetMapping("/redis/id/{id}")
+    public ResponseEntity findInvite(@PathVariable int id) {
+        inviteService.findCode("606777");
+
+        Invite code = inviteService.findCode(id);
+        log.info("코드 : {}", code.getCode());
+        log.info("유저id : {}", code.getUserId());
+        return null;
+    }
 }
