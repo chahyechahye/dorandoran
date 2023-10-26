345201b0e796370bfc813d0b801b75e96bdb0584
cje9717@naver.com <cje9717@naver.com>
2023-10-26 11:03:19 +0900

    [S09P31B108-314] feat : 아동 캐릭터 페이지 구현

---
+++ frontend/src/pages/children/character/index.tsx
@@ -0,0 +1,67 @@
+import styled from "styled-components";
+import background from "@/assets/img/background/background.jpg";
+import CharacterPage from "@/pages/common/character";
+import ClickButton from "@/components/clickButton";
+
+const Background = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-image: url(${background});
+  background-size: cover;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const CharacterModal = styled.div``;
+
+const ModalBlack = styled.div`
+  background-color: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+`;
+
+const ClickButtonContainer = styled.div`
+  position: relative;
+  z-index: 1;
+  margin-top: 7vh;
+`;
+
+const ChildrenLoginPage = () => {
+  return (
+    <Background>
+      <ContentContainer>
+        <CharacterModal>
+          <ModalBlack />
+          <CharacterPage />
+
+          <ClickButtonContainer>
+            <ClickButton
+              width="70vh"
+              height="9vh"
+              backgroundColor="#FC7292"
+              fontColor="white"
+              fontSize="4.5vh"
+              text="이 친구랑 놀래요!"
+            ></ClickButton>
+          </ClickButtonContainer>
+        </CharacterModal>
+      </ContentContainer>
+    </Background>
+  );
+};
+
+export default ChildrenLoginPage;
