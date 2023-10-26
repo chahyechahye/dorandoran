12c558dca1a78b6537b990908dd3109640721122
한성현 <hsh5349@kakao.com>
2023-10-26 17:05:32 +0900

    [S09P31B108-243] feat : userId로 초대코드 조회 쿼리 생성

--- backend/src/main/java/com/doran/redis/invite/repository/InviteRepository.java
+++ backend/src/main/java/com/doran/redis/invite/repository/InviteRepository.java
@@ -1,5 +1,7 @@
 package com.doran.redis.invite.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,6 @@ import com.doran.redis.invite.key.Invite;
 
 @Repository
 public interface InviteRepository extends CrudRepository<Invite, String> {
+
+    Optional<Invite> findInviteByUserId(int userId);
 }
