4bcb4b087aee97ff01f42afcdd52f190d3814fea
한성현 <hsh5349@kakao.com>
2023-10-26 12:28:25 +0900

    [S09P31B108-229] feat : 커스텀디테일서비스 직접 구현

--- backend/src/main/java/com/doran/user/service/CustomUserDetailService.java
+++ backend/src/main/java/com/doran/user/service/CustomUserDetailService.java
@@ -1,21 +1,45 @@
 package com.doran.user.service;
 
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import lombok.RequiredArgsConstructor;
+import com.doran.parent.type.Provider;
+import com.doran.user.dto.req.UserTokenBaseDto;
+import com.doran.user.type.Roles;
 
-@RequiredArgsConstructor
+import io.jsonwebtoken.Claims;
+import lombok.extern.slf4j.Slf4j;
+
 @Service
-public class CustomUserDetailService implements UserDetailsService {
+@Slf4j
+public class CustomUserDetailService {
 
-	private final UserService userService;
+	public UserDetails loadUserByUsername(Claims userInfo) throws UsernameNotFoundException {
+		Object userRole = userInfo.get("userRole");
+		Object provider = userInfo.get("provider");
+		Provider provider1 = Provider.valueOf(provider.toString());
+		Roles roles = Roles.valueOf(userRole.toString());
 
-	@Override
-	public UserDetails loadUserByUsername(String id) throws UsernameNotFoundException {
+		int userId = (int)userInfo.get("userId");
+		int parentId = (int)userInfo.get("parentId");
+		// int childId = (int)userInfo.get("childId");
+		// int selectProfileId = (int)userInfo.get("selectProfileId");
+		log.info("userId : {}", userId);
+		log.info("parentId : {}", parentId);
+		// log.info("childId : {}", childId);
+		// log.info("selectProfileId : {}", selectProfileId);
 
-		return userService.findUser(Integer.parseInt(id));
+		UserTokenBaseDto build = UserTokenBaseDto.builder()
+			.userId(userId)
+			// .childId(childId)
+			.parentId(parentId)
+			// .selectProfileId(selectProfileId)
+			.provider(provider1)
+			.userRole(roles)
+			.build();
+		log.info(build.toString());
+
+		return build;
 	}
 }
