2e586f0397ef0e36319ebb607bdae8c1e109a6d5
한성현 <hsh5349@kakao.com>
2023-10-26 12:47:03 +0900

    [S09P31B108-229] feat : 시큐리티에 담길 객체 변경

---
+++ backend/src/main/java/com/doran/utils/common/UserInfo.java
@@ -0,0 +1,59 @@
+package com.doran.utils.common;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.doran.user.type.Roles;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class UserInfo implements UserDetails {
+	private int userId;
+	private Integer selectProfileId;
+	private Roles userRole;
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		List<GrantedAuthority> authorities = new ArrayList<>();
+		authorities.add(new SimpleGrantedAuthority(this.userRole.getRole()));
+		return authorities;
+	}
+
+	@Override
+	public String getPassword() {
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+		return null;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return false;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return false;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return false;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return false;
+	}
+}
