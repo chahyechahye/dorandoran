79eabf7f893e21e77f2f8454b475d89617c8db26
한성현 <hsh5349@kakao.com>
2023-10-26 17:06:13 +0900

    [S09P31B108-243] feat : code 또는 userId로 초대코드 객체 조회

--- backend/src/main/java/com/doran/redis/invite/service/InviteService.java
+++ backend/src/main/java/com/doran/redis/invite/service/InviteService.java
@@ -1,18 +1,23 @@
 package com.doran.redis.invite.service;
 
 import java.util.Locale;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
 import com.doran.redis.invite.key.Invite;
 import com.doran.redis.invite.mapper.InviteMapper;
 import com.doran.redis.invite.repository.InviteRepository;
+import com.doran.utils.exception.dto.CustomException;
+import com.doran.utils.exception.dto.ErrorCode;
 import com.github.javafaker.Faker;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class InviteService {
     private final InviteRepository inviteRepository;
     private final InviteMapper inviteMapper;
@@ -27,4 +32,23 @@ public class InviteService {
     }
 
     //조회
+    public void findCode(String code) {
+        log.info("코드로 조회 : {}", code);
+        Optional<Invite> findInvite = inviteRepository.findById(code);
+
+        validCode(findInvite);
+    }
+
+    public Invite findCode(int userId) {
+        Optional<Invite> findInvite = inviteRepository.findInviteByUserId(userId);
+
+        return validCode(findInvite);
+    }
+
+    //유효한지 검사
+    public Invite validCode(Optional<Invite> findInvite) {
+        return findInvite
+            .orElseThrow(() -> new CustomException(ErrorCode.INVITE_CODE_NOT_FOUND));
+    }
+
 }
