509a8b641ef8c1c357409ffa193f0022996e334d
황준혁 <hyug0819@gmail.com>
2023-10-26 14:03:53 +0900

    [S09P31B108-270] fix : CustomResponseEntity로 변경

--- backend/src/main/java/com/doran/animal/controller/AnimalController.java
+++ backend/src/main/java/com/doran/animal/controller/AnimalController.java
@@ -2,12 +2,12 @@ package com.doran.animal.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.doran.animal.dto.res.AnimalListDto;
 import com.doran.animal.service.AnimalService;
+import com.doran.response.CommonResponseEntity;
+import com.doran.response.SuccessCode;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,8 +21,8 @@ public class AnimalController {
 	private final AnimalService animalService;
 
 	@GetMapping("/all")
-	public ResponseEntity<AnimalListDto> animalList() {
-		return ResponseEntity.ok(animalService.selectAllAnimal());
+	public ResponseEntity<?> animalList() {
+		return CommonResponseEntity.getResponseEntity(SuccessCode.OK, animalService.selectAllAnimal());
 	}
 
 	@PostMapping("")
