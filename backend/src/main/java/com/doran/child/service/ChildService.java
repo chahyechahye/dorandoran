f333d72510c38b763f80cdbfd6b34f325fe015e7
황준혁 <hyug0819@gmail.com>
2023-10-27 17:50:29 +0900

    [S09P31B108-268] feat : Child Service 작성

--- backend/src/main/java/com/doran/child/service/ChildService.java
+++ backend/src/main/java/com/doran/child/service/ChildService.java
@@ -1,12 +1,17 @@
 package com.doran.child.service;
 
+import org.springframework.stereotype.Service;
+
 import com.doran.child.ChildMapper;
+import com.doran.child.dto.res.ChildDto;
 import com.doran.child.entity.Child;
 import com.doran.child.repository.ChildRepository;
 import com.doran.parent.entity.Parent;
 import com.doran.user.entity.User;
+import com.doran.utils.exception.dto.CustomException;
+import com.doran.utils.exception.dto.ErrorCode;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
@@ -18,5 +23,10 @@ public class ChildService {
     public void saveChild(Parent parent, User user) {
         Child child = childMapper.toChild(parent, user);
         childRepository.save(child);
+    }
+
+    public ChildDto findChildByParentUserId(int userId) {
+        return childRepository.findChildToParentUserId(userId)
+                              .orElseThrow(() -> new CustomException(ErrorCode.CHILD_NOT_FOUND));
     }
 }
