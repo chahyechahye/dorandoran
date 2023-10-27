63bc69dc1df3b5361f1452b0f28175c97e4b28c1
HongJiMin <baker0929@naver.com>
2023-10-27 16:38:37 +0900

    [S09P31B108-250] feat : 앨범 Entity 생성

---
+++ backend/src/main/java/com/doran/album/entity/Album.java
@@ -0,0 +1,33 @@
+package com.doran.album.entity;
+
+import com.doran.child.entity.Child;
+import com.doran.parent.entity.Parent;
+import com.doran.user.entity.User;
+import com.doran.utils.sens.BaseTimeEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "album")
+public class Album extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "album_id")
+    private int id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Parent parent;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "child_id")
+    private Child child;
+}
