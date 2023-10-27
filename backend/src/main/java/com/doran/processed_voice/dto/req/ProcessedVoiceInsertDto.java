3f74e46f92ae194b884f3bf9570b0ab6b0b750a8
이정섭 <dark6ro@naver.com>
2023-10-27 13:45:41 +0900

    [S09P31B108-212] feat : insert 백업

--- backend/src/main/java/com/doran/processed_voice/dto/req/ProcessedVoiceInsertDto.java
+++ backend/src/main/java/com/doran/processed_voice/dto/req/ProcessedVoiceInsertDto.java
@@ -12,5 +12,6 @@ import lombok.Setter;
 @RequiredArgsConstructor
 public class ProcessedVoiceDto {
     int contentId; // 목소리 등록할 땐 null 혹은 그외 다른 값, 낭독할 땐 동화책 컨텐츠 id 반환
+    String voiceType;
     private MultipartFile voice;
 }
