7b1281da53e26427c06df1c1c28b8ca5220913e3
한성현 <hsh5349@kakao.com>
2023-10-30 13:48:25 +0900

    [S09P31B108-336] feat : 인증 처리 진행

--- backend/src/main/java/com/doran/user/service/CustomUserDetailService.java
+++ backend/src/main/java/com/doran/user/service/CustomUserDetailService.java
@@ -6,28 +6,48 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.doran.user.repository.UserRepository;
 import com.doran.user.type.Roles;
 import com.doran.utils.common.UserInfo;
+import com.doran.utils.exception.dto.CustomException;
+import com.doran.utils.exception.dto.ErrorCode;
 
 import io.jsonwebtoken.Claims;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Service
+@RequiredArgsConstructor
 @Slf4j
 public class CustomUserDetailService {
+    private final UserRepository userRepository;
 
     public UserDetails loadUserByUsername(Claims userInfo) throws UsernameNotFoundException {
-        UserInfo build = new UserInfo();
+        UserInfo build;
 
         Roles roles = Roles.valueOf(userInfo.get("userRole").toString());
-        build.setUserRole(roles);
-        build.setUserId((int)userInfo.get("userId"));
-        //있으면 넣고 없으면 안넣음
-        Optional.ofNullable(userInfo.get("selectProfileId"))
-            .ifPresent(v -> build.setSelectProfileId((int)v));
+        int userId = (int)userInfo.get("userId");
+        //role이 아이일 때
+        if (roles.equals(Roles.CHILD)) {
+            log.info("아이 토큰");
+            int selectProfileId = (int)userInfo.get("selectProfileId");
+            Optional<UserInfo> findUser = userRepository.findUser(userId, selectProfileId, roles);
+
+            build = validUserInfo(findUser);
+        } else {
+            log.info("부모 or 관리자 토큰");
+            Optional<UserInfo> findUser = userRepository.findUser(userId, roles);
+
+            build = validUserInfo(findUser);
+        }
 
         log.info("userInfo : {}", build);
 
         return build;
+    }
+
+    public UserInfo validUserInfo(Optional<UserInfo> findUser) {
+        return findUser
+            .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
     }
 }
