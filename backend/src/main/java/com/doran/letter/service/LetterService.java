61d3c793aabb2c79a4c58d8c6b8477b1e7dd9c79
이정섭 <dark6ro@naver.com>
2023-10-30 15:52:19 +0900

    [S09P31B108-355] comment : 편지 insert 주석 및 백업

--- backend/src/main/java/com/doran/letter/service/LetterService.java
+++ backend/src/main/java/com/doran/letter/service/LetterService.java
@@ -2,17 +2,29 @@ package com.doran.letter.service;
 
 import org.springframework.stereotype.Service;
 
+import com.doran.letter.dto.req.LetterInsertDto;
 import com.doran.letter.repository.LetterRepository;
+import com.doran.parent.service.ParentService;
+import com.doran.profile.entity.Profile;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class LetterService{
     private final LetterRepository letterRepository;
+    // private final ParentService parentService;
+    // private final ProfileService profileService;
     // 부모한테 보낸 편지 조회
 
     // 자식(Profile)한테 보낸 편지 조회
 
     // 편지 등록
+    public void insertLetter(LetterInsertDto letterInsertDto){
+        // 편지 저장 시 파일 이름
+        String filename = "";
+
+    }
 }
