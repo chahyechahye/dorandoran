eb0dd29a57394cabda628a61ff209816102648df
황준혁 <hyug0819@gmail.com>
2023-10-30 16:49:21 +0900

    [S09P31B108-401] feat : Favorite Repository 구현

--- backend/src/main/java/com/doran/favorite/repository/FavoriteRepositoryCustom.java
+++ backend/src/main/java/com/doran/favorite/repository/FavoriteRepositoryCustom.java
@@ -1,4 +1,9 @@
 package com.doran.favorite.repository;
 
+import java.util.List;
+
+import com.doran.book.entity.Book;
+
 public interface FavoriteRepositoryCustom {
+    List<Book> selectChildFavoriteBook(int profileId);
 }
