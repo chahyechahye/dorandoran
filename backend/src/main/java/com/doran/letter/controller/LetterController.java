cd57ef525378865d294eddf4eede0eb953d5608d
이정섭 <dark6ro@naver.com>
2023-10-31 13:01:35 +0900

    [S09P31B108-355] fix : Controller의 로직 Service로 이전

--- backend/src/main/java/com/doran/letter/controller/LetterController.java
+++ backend/src/main/java/com/doran/letter/controller/LetterController.java
@@ -1,6 +1,7 @@
 package com.doran.letter.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,37 +31,14 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/api/letter")
 @RequiredArgsConstructor
 public class LetterController {
-    private final ParentService parentService; // 부모 아이 판별을 위해 import
-    private final ParentRepository parentRepository;
-    private final ProfileRepository profileRepository;
-    private final BucketMapper bucketMapper;
-    private final BucketService bucketService;
-    private final LetterMapper letterMapper;
     private final LetterService letterService;
     // 편지 조회
 
 
     // 편지 등록
+    @PostMapping("")
     public ResponseEntity<?> insertLetter(LetterInsertDto letterInsertDto){
-        UserInfo userInfo = Auth.getInfo();
-        Parent parent = null;
-        Profile profile = null;
-        if(parentService.checkParent(userInfo.getUserRole().getRole())){
-            // 보내는 사람 프로필(아이), 받는 사람 부모일 때
-            parent = parentRepository.findParentByProfileId(letterInsertDto.getProfileId())
-                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
-            profile = profileRepository.findById(letterInsertDto.getProfileId())
-                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
-        }else{
-            // 보내는 사람 부모, 받는 사람 프로필(아이)일 때
-            parent = parentRepository.findParentByUserId(userInfo.getUserId())
-                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
-            profile = profileRepository.findById(letterInsertDto.getProfileId())
-                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
-        }
-        String contentUrl = bucketService.insertFile(bucketMapper.toInsertDto(letterInsertDto.getContent(), "letter"));
-        Letter letter = letterMapper.insertLettertoLetter(letterInsertDto,parent,profile,contentUrl);
-        letterService.insertLetter(letter);
+        Letter letter = letterService.insertLetter(letterInsertDto);
         return CommonResponseEntity.getResponseEntity(SuccessCode.SUCCESS_CODE, null);
     }
 }
--- backend/src/main/java/com/doran/letter/service/LetterService.java
+++ backend/src/main/java/com/doran/letter/service/LetterService.java
@@ -4,9 +4,19 @@ import org.springframework.stereotype.Service;
 
 import com.doran.letter.dto.req.LetterInsertDto;
 import com.doran.letter.entity.Letter;
+import com.doran.letter.mapper.LetterMapper;
 import com.doran.letter.repository.LetterRepository;
+import com.doran.parent.entity.Parent;
 import com.doran.parent.service.ParentService;
 import com.doran.profile.entity.Profile;
+import com.doran.profile.repository.ProfileRepository;
+import com.doran.profile.service.ProfileService;
+import com.doran.utils.auth.Auth;
+import com.doran.utils.bucket.mapper.BucketMapper;
+import com.doran.utils.bucket.service.BucketService;
+import com.doran.utils.common.UserInfo;
+import com.doran.utils.exception.dto.CustomException;
+import com.doran.utils.exception.dto.ErrorCode;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,14 +25,37 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 @Slf4j
 public class LetterService{
+    private final ParentService parentService; // 부모 아이 판별을 위해 import
+    private final ProfileService profileService;
+    private final BucketMapper bucketMapper;
+    private final BucketService bucketService;
+    private final LetterMapper letterMapper;
     private final LetterRepository letterRepository;
     // 부모한테 보낸 편지 조회
 
     // 자식(Profile)한테 보낸 편지 조회
 
     // 편지 등록
-    public void insertLetter(Letter letter){
+    public Letter insertLetter(LetterInsertDto letterInsertDto){
+        UserInfo userInfo = Auth.getInfo();
+        Parent parent = null;
+        Profile profile = null;
+        int receiverId=0, senderId=0;
+        profile = profileService.findProfileById(letterInsertDto.getProfileId());
+        if(parentService.checkParent(userInfo.getUserRole().getRole())){
+            // 보내는 사람 프로필(아이), 받는 사람 부모일 때
+            parent = parentService.findParentByProfileId(letterInsertDto.getProfileId());
+            receiverId = parent.getId();
+            senderId = profile.getId();
+        }else{
+            // 보내는 사람 부모, 받는 사람 프로필(아이)일 때
+            parent = parentService.findParentByUserId(userInfo.getUserId());
+            receiverId = profile.getId();
+            senderId = parent.getId();
+        }
+        String contentUrl = bucketService.insertFile(bucketMapper.toInsertDto(letterInsertDto.getContent(), "letter"));
+        Letter letter = letterMapper.insertLettertoLetter(letterInsertDto,parent,profile,contentUrl,receiverId,senderId);
         // 편지 저장 시 파일 이름
-        letterRepository.save(letter);
+        return letterRepository.save(letter);
     }
 }
