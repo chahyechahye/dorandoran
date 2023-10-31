6d47c50163aade45f2fc33911fa8be5f77637efb
이정섭 <dark6ro@naver.com>
2023-10-31 15:25:25 +0900

    [S09P31B108-355] feat : letter 검색 querydsl 추가

--- backend/src/main/java/com/doran/letter/repository/LetterRepositoryCustom.java
+++ backend/src/main/java/com/doran/letter/repository/LetterRepositoryCustom.java
@@ -1,4 +1,12 @@
 package com.doran.letter.repository;
 
+import java.util.List;
+
+import com.doran.letter.entity.Letter;
+
 public interface LetterRepositoryCustom {
+    List<Letter> findAllLetterByParentId(int parentId);
+    Letter findLetterByParentId(int parentId);
+    List<Letter> findAllLetterByProfileId(int profileId);
+    Letter findLetterByProfileId(int profileId);
 }
