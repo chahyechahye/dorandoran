05aab9fb4ea53e5883a8599135366f56e8cbe8f0
이정섭 <dark6ro@naver.com>
2023-10-31 15:26:07 +0900

    [S09P31B108-355] feat : letter 검색 Mapper 추가

--- backend/src/main/java/com/doran/letter/mapper/LetterMapper.java
+++ backend/src/main/java/com/doran/letter/mapper/LetterMapper.java
@@ -4,6 +4,7 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 import com.doran.letter.dto.req.LetterInsertDto;
+import com.doran.letter.dto.res.LetterResDto;
 import com.doran.letter.entity.Letter;
 import com.doran.parent.entity.Parent;
 import com.doran.profile.entity.Profile;
@@ -14,6 +15,11 @@ import lombok.RequiredArgsConstructor;
 public interface LetterMapper {
     @Mapping(target = "id", ignore = true)
     Letter insertLettertoLetter(LetterInsertDto letterInsertDto, Parent parent, Profile profile, String contentUrl, int receiverId, int senderId);
+
+
+
+    LetterResDto parentLetterToResDto(Letter letter);
+    LetterResDto childLetterToResDto(Letter letter);
 
 
 }
