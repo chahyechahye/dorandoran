c87bef66efa37a7e5f3d94cf9a109d16799c458c
이정섭 <dark6ro@naver.com>
2023-10-31 13:08:41 +0900

    [S09P31B108-355] fix : ResDto 수정

--- backend/src/main/java/com/doran/letter/dto/res/LetterResDto.java
+++ backend/src/main/java/com/doran/letter/dto/res/LetterResDto.java
@@ -1,6 +1,7 @@
 package com.doran.letter.dto.res;
 
-import com.google.type.DateTime;
+import java.time.LocalDateTime;
+
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -12,6 +13,6 @@ import lombok.Setter;
 public class LetterResDto {
     private String sender; // 편지 작성자
     private String contentUrl;
-    private DateTime createdDate; // 작성 날짜
+    private LocalDateTime createdDate; // 작성 날짜
 
 }
