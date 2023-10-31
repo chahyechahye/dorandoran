bb42ee4c551e2a7c3dd7fe7d9306fc5cbf3381ac
이정섭 <dark6ro@naver.com>
2023-10-31 13:00:40 +0900

    [S09P31B108-355] feat : entity 반환하는 parent id기반 검색, profile id 검색 기능 추가

--- backend/src/main/java/com/doran/profile/service/ProfileService.java
+++ backend/src/main/java/com/doran/profile/service/ProfileService.java
@@ -2,6 +2,7 @@ package com.doran.profile.service;
 
 import com.doran.profile.dto.res.ProfileDto;
 import com.doran.profile.dto.res.ProfileListDto;
+import com.doran.profile.entity.Profile;
 import com.doran.utils.common.UserInfo;
 
 public interface ProfileService {
@@ -9,6 +10,7 @@ public interface ProfileService {
     ProfileListDto selectAllProfile(int childId);
 
     ProfileDto selectProfile(int childId, int profileId);
+    Profile findProfileById(int profileId);
 
     void createChildProfile(int childId, String name);
 
--- backend/src/main/java/com/doran/profile/service/ProfileServiceImpl.java
+++ backend/src/main/java/com/doran/profile/service/ProfileServiceImpl.java
@@ -70,6 +70,12 @@ public class ProfileServiceImpl implements ProfileService {
     }
 
     @Override
+    public Profile findProfileById(int profileId){
+        return profileRepository.findById(profileId)
+            .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+    }
+
+    @Override
     public void createChildProfile(int childId, String name) {
         Profile profile = new Profile();
         profile.setAnimal(
@@ -93,4 +99,5 @@ public class ProfileServiceImpl implements ProfileService {
         profile.setAnimal(animal);
         profileRepository.save(profile);
     }
+
 }
