fd68ed99ed872209c1b372e3dfa9e0fb98b21a70
Ssuhyeong <tngud124@gmail.com>
2023-11-01 14:49:54 +0900

    [S09P31B108-389] feat : 로그인 관련 로직 구현

--- frontend/src/pages/parent/login/index.tsx
+++ frontend/src/pages/parent/login/index.tsx
@@ -31,10 +31,24 @@ const LoginImage = styled.img`
 `;
 
 const ParentLoginPage = () => {
+  const onSocialButtonClick = () => {
+    const OAUTH2_REDIERECT_URI = `${process.env.REACT_APP_BASE_URL}/oauth/redirect`;
+    const API_KEY = "f9a75e5f47148dc0aa986669c29c0d03";
+    const AUTH_URL = `${process.env.REACT_APP_SERVER_URL}?response_type=code&client_id=${API_KEY}&redirect_uri=${OAUTH2_REDIERECT_URI}`;
+    window.location.href = AUTH_URL;
+
+    const code = new URL(window.location.href).searchParams.get("code");
+    console.log(code);
+  };
+
   return (
     <Container>
       <Image src={Logo} alt="Background" />
-      <LoginImage src={kakaoLogo} alt="Background" />
+      <LoginImage
+        src={kakaoLogo}
+        alt="Background"
+        onClick={onSocialButtonClick}
+      />
       <LoginImage src={googleLogo} alt="Background" />
     </Container>
   );
