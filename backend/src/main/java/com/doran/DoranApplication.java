bf98426d79bd8de7b297d5ab0a903dc4c9eba061
한성현 <hsh5349@kakao.com>
2023-11-01 12:01:10 +0900

    [S09P31B108-347] feat : redis 설정 추가

--- backend/src/main/java/com/doran/DoranApplication.java
+++ backend/src/main/java/com/doran/DoranApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.redis.core.RedisKeyValueAdapter;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
@@ -12,15 +14,16 @@ import jakarta.persistence.EntityManager;
 
 @EnableJpaAuditing
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@EnableRedisRepositories(enableKeyspaceEvents = RedisKeyValueAdapter.EnableKeyspaceEvents.ON_STARTUP)
 public class DoranApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DoranApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DoranApplication.class, args);
+    }
 
-	@Bean
-	JPAQueryFactory jpaQueryFactory(EntityManager entityManager) {
-		return new JPAQueryFactory(entityManager);
-	}
+    @Bean
+    JPAQueryFactory jpaQueryFactory(EntityManager entityManager) {
+        return new JPAQueryFactory(entityManager);
+    }
 
 }
