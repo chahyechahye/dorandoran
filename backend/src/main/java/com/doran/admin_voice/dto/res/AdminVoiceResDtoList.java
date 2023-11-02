e6788aa3ac5a30c1c8b4b03478095e498daac163
이정섭 <dark6ro@naver.com>
2023-11-02 14:39:31 +0900

    [S09P31B108-378] feat : 관리자 목소리 검색 dto 추가

---
+++ backend/src/main/java/com/doran/admin_voice/dto/res/AdminVoiceResDtoList.java
@@ -0,0 +1,9 @@
+package com.doran.admin_voice.dto.res;
+
+import java.util.List;
+
+public class AdminVoiceResDtoList {
+    // 해당 컨텐츠의 목소리 전원 반환할 때 필요
+    private int size;
+    private List<AdminVoiceResDto> adminVoiceResDtoList;
+}
