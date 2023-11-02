41bd619d615198b2ab7f20e72304ba6cd4075cc5
HongJiMin <baker0929@naver.com>
2023-11-02 14:01:01 +0900

    [S09P31B108-340] feat : 모델 생성요청 DTO 생성

---
+++ backend/src/main/java/com/doran/utils/rabbitmq/dto/req/ModelReqMessage.java
@@ -0,0 +1,23 @@
+package com.doran.utils.rabbitmq.dto.req;
+
+import java.util.List;
+
+import com.doran.raw_voice.entity.RawVoice;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class ModelReqMessage {
+    private int userId;
+    private List<RawVoice> rawVoiceList;
+}
