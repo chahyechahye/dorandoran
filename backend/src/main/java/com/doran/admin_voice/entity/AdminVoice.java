0910abc654b0a84eaf0132ac265338ecf2c22e07
이정섭 <dark6ro@naver.com>
2023-11-02 12:52:40 +0900

    [S09P31B108-378] feat : 관리자 목소리 entity 추가

---
+++ backend/src/main/java/com/doran/admin_voice/entity/AdminVoice.java
@@ -0,0 +1,41 @@
+package com.doran.admin_voice.entity;
+
+import com.doran.admin_voice.type.Genders;
+import com.doran.content.entity.Content;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table("admin_voice")
+@AllArgsConstructor
+public class AdminVoice {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "av_id")
+    private int id;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Column(name = "content_id")
+    private Content content;
+
+    private String voiceUrl;
+
+    @Enumerated(EnumType.STRING)
+    private Genders gender;
+
+}
