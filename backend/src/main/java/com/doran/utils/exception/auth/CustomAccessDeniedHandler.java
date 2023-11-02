ce17387c91cbfa46b66ad0053eea405243ce7a31
한성현 <hsh5349@kakao.com>
2023-11-02 12:53:08 +0900

    [S09P31B108-354] feat : 인가 에러 핸들링

--- backend/src/main/java/com/doran/utils/exception/auth/CustomAccessDeniedHandler.java
+++ backend/src/main/java/com/doran/utils/exception/auth/CustomAccessDeniedHandler.java
@@ -2,9 +2,16 @@ package com.doran.utils.exception.auth;
 
 import java.io.IOException;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
+
+import com.doran.utils.exception.dto.ErrorCode;
+import com.doran.utils.exception.dto.ErrorResponseEntity;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -18,5 +25,16 @@ public class CustomAccessDeniedHandler implements AccessDeniedHandler {
     public void handle(HttpServletRequest request, HttpServletResponse response,
         AccessDeniedException accessDeniedException) throws IOException, ServletException {
         log.info("권한 핸들러 진입");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        response.setCharacterEncoding("utf-8");
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        ResponseEntity<ErrorResponseEntity> responseEntity = ErrorResponseEntity.toResponseEntity(
+            ErrorCode.AUTHENTICATION_FORBIDDEN);
+        String responseBody = objectMapper.writeValueAsString(responseEntity.getBody());
+
+        response.getWriter().write(responseBody);
     }
 }
