078ac49372bdc0dbf8122ed7c2e4818a1ffd249f
이정섭 <dark6ro@naver.com>
2023-11-03 11:15:21 +0900

    [S09P31B108-378] fix : 관리자 목소리 url 수정

--- backend/src/main/java/com/doran/admin_voice/controller/AdminVoiceController.java
+++ backend/src/main/java/com/doran/admin_voice/controller/AdminVoiceController.java
@@ -1,16 +1,19 @@
 package com.doran.admin_voice.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.doran.admin_voice.dto.req.AdminVoiceInsertDto;
 import com.doran.admin_voice.dto.res.AdminVoiceResDto;
 import com.doran.admin_voice.dto.res.AdminVoiceResDtoList;
 import com.doran.admin_voice.service.AdminVoiceService;
+import com.doran.admin_voice.type.Genders;
 import com.doran.utils.response.CommonResponseEntity;
 import com.doran.utils.response.SuccessCode;
 
@@ -24,29 +27,29 @@ import lombok.extern.slf4j.Slf4j;
 public class AdminVoiceController {
     private final AdminVoiceService adminVoiceService;
 
-    // content 단위 남성 관리자 목소리
-    @GetMapping("/male/{content_id}")
-    public ResponseEntity<?> getMaleAdminVoiceByContentId(@PathVariable(value="content_id") int contentId){
-        AdminVoiceResDto adminVoiceResDto = adminVoiceService.findMaleAdminVoiceByContentId(contentId);
+    // content 단위 관리자 목소리
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping("/{content_id}")
+    public ResponseEntity<?> getAdminVoiceByContentId(@PathVariable(value="content_id") int contentId,
+                                                @RequestParam(name="gender") Genders gender){
+        AdminVoiceResDto adminVoiceResDto = adminVoiceService.findAdminVoiceByContentId(gender,contentId);
         return CommonResponseEntity.getResponseEntity(SuccessCode.SUCCESS_CODE,adminVoiceResDto);
     }
-    // content 단위 여성 관리자 목소리
-    @GetMapping("/female/{content_id}")
-    public ResponseEntity<?> getFemaleAdminVoiceByContentId(@PathVariable(value="content_id") int contentId){
-        AdminVoiceResDto adminVoiceResDto = adminVoiceService.findFemaleAdminVoiceByContentId(contentId);
-        return CommonResponseEntity.getResponseEntity(SuccessCode.SUCCESS_CODE,adminVoiceResDto);
-    }
-    // book 단위 남성 관리자 목소리
-    @GetMapping("/male/{book_id}")
-    public ResponseEntity<?> getMaleAdminVoiceByBookId(@PathVariable(value="book_id") int bookId){
-        AdminVoiceResDtoList adminVoiceResDtoList = adminVoiceService.findMaleAdminVoiceByBookId(bookId);
+    // book 단위 관리자 목소리
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping("/book/{book_id}")
+    public ResponseEntity<?> getAdminVoiceByBookId(@PathVariable(value="book_id") int bookId,
+                                                @RequestParam(name="gender") Genders gender){
+        log.info("Query start " + bookId + " " + gender);
+        AdminVoiceResDtoList adminVoiceResDtoList = adminVoiceService.findAdminVoiceByBookId(gender,bookId);
+        log.info("Query end");
         return CommonResponseEntity.getResponseEntity(SuccessCode.SUCCESS_CODE,adminVoiceResDtoList);
     }
-    // book 단위 여성 관리자 목소리
-    @GetMapping("/female/{book_id}")
-    public ResponseEntity<?> getFemaleAdminVoiceByBookId(@PathVariable(value="book_id") int bookId){
-        AdminVoiceResDtoList adminVoiceResDtoList = adminVoiceService.findFemaleAdminVoiceByBookId(bookId);
-        return CommonResponseEntity.getResponseEntity(SuccessCode.SUCCESS_CODE,adminVoiceResDtoList);
+    // 관리자 목소리 등록
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PostMapping("")
+    public ResponseEntity<?> insertAdminVoice(AdminVoiceInsertDto adminVoiceInsertDto){
+        adminVoiceService.insertAdminVoice(adminVoiceInsertDto);
+        return CommonResponseEntity.getResponseEntity(SuccessCode.SUCCESS_CODE);
     }
-
 }
