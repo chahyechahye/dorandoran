13bde42aa9c49cbfaad07d8b1193141d1f0bdcf1
HongJiMin <baker0929@naver.com>
2023-11-03 17:21:23 +0900

    [S09P31B108-327] feat : 번호 등록 API 구현

--- backend/src/main/java/com/doran/raw_voice/controller/RawVoiceController.java
+++ backend/src/main/java/com/doran/raw_voice/controller/RawVoiceController.java
@@ -5,12 +5,16 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.doran.raw_voice.dto.req.RawVoiceInsertDto;
+import com.doran.raw_voice.dto.req.TelInsertDto;
 import com.doran.raw_voice.dto.res.RawVoiceListDto;
 import com.doran.raw_voice.service.RawVoiceService;
+import com.doran.redis.tel.service.TelService;
+import com.doran.utils.auth.Auth;
 import com.doran.utils.response.CommonResponseEntity;
 import com.doran.utils.response.SuccessCode;
 
@@ -23,6 +27,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class RawVoiceController {
     private final RawVoiceService rawVoiceService;
+    private final TelService telService;
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping("")
@@ -30,19 +35,40 @@ public class RawVoiceController {
         log.info("Raw Voice (원본 목소리) 리스트 호출");
         return CommonResponseEntity.getResponseEntity(SuccessCode.SUCCESS_CODE, rawVoiceService.getRawVoiceAll());
     }
+
     // 유저 목소리 검색
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping("/{user_id}")
-    public ResponseEntity<?> getRawVoiceListByUserId(@PathVariable(value="user_id") int userId){
+    public ResponseEntity<?> getRawVoiceListByUserId(@PathVariable(value = "user_id") int userId) {
         RawVoiceListDto rawVoiceListDto = rawVoiceService.getRawVoiceByUserId(userId);
         return CommonResponseEntity.getResponseEntity(SuccessCode.SUCCESS_CODE, rawVoiceListDto);
     }
+
     // 원본 목소리 등록 (부모 실제 녹음)
     @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_PARENT')") // 관리자, 부모만 등록 가능
     @PostMapping("")
     public ResponseEntity<?> insertRawVoice(RawVoiceInsertDto rawVoiceInsertDto) {
         log.info("Raw Voice (원본 목소리) 추가");
         rawVoiceService.insertRawVoice(rawVoiceInsertDto);
-        return CommonResponseEntity.getResponseEntity(SuccessCode.SUCCESS_CODE,null);
+        return CommonResponseEntity.getResponseEntity(SuccessCode.SUCCESS_CODE, null);
+    }
+
+    //녹음 완료 요청 API
+    @GetMapping("/complete")
+    public ResponseEntity<?> completeRecord() {
+        log.info("녹음 완료 요청 컨트롤러");
+
+        //model pub 호출
+
+        return CommonResponseEntity.getResponseEntity(SuccessCode.SUCCESS_CODE);
+    }
+
+    //모델 생성시 알림이 갈 번호 등록
+    @PostMapping("/tel")
+    public ResponseEntity<?> insertTel(@RequestBody TelInsertDto telInsertDto) {
+        log.info("번호 등록 컨트롤러");
+        //redis 저장
+        telService.save(Auth.getInfo().getUserId(), telInsertDto.getTel());
+        return CommonResponseEntity.getResponseEntity(SuccessCode.SUCCESS_CODE);
     }
 }
---
+++ backend/src/main/java/com/doran/raw_voice/dto/req/TelInsertDto.java
@@ -0,0 +1,13 @@
+package com.doran.raw_voice.dto.req;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class TelInsertDto {
+    private String tel;
+}
