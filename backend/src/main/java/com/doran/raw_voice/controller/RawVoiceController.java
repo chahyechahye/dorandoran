0d46f5f83247fa7869b87075ea64e48abdaa8773
이정섭 <dark6ro@naver.com>
2023-11-03 15:37:56 +0900

    [S09P31B108-378] feat : Genders 추가 및 userId기반 검색기능 추가

--- backend/src/main/java/com/doran/raw_voice/controller/RawVoiceController.java
+++ backend/src/main/java/com/doran/raw_voice/controller/RawVoiceController.java
@@ -3,11 +3,13 @@ package com.doran.raw_voice.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.doran.raw_voice.dto.req.RawVoiceInsertDto;
+import com.doran.raw_voice.dto.res.RawVoiceListDto;
 import com.doran.raw_voice.service.RawVoiceService;
 import com.doran.utils.response.CommonResponseEntity;
 import com.doran.utils.response.SuccessCode;
@@ -28,8 +30,14 @@ public class RawVoiceController {
         log.info("Raw Voice (원본 목소리) 리스트 호출");
         return CommonResponseEntity.getResponseEntity(SuccessCode.SUCCESS_CODE, rawVoiceService.getRawVoiceAll());
     }
-
-    //
+    // 유저 목소리 검색
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping("/{user_id}")
+    public ResponseEntity<?> getRawVoiceListByUserId(@PathVariable(value="user_id") int userId){
+        RawVoiceListDto rawVoiceListDto = rawVoiceService.getRawVoiceByUserId(userId);
+        return CommonResponseEntity.getResponseEntity(SuccessCode.SUCCESS_CODE, rawVoiceListDto);
+    }
+    // 원본 목소리 등록 (부모 실제 녹음)
     @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_PARENT')") // 관리자, 부모만 등록 가능
     @PostMapping("")
     public ResponseEntity<?> insertRawVoice(RawVoiceInsertDto rawVoiceInsertDto) {
