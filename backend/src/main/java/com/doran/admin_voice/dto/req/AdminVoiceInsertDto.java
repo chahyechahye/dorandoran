6ecdacf8f47e50cd99070be7dfc34fbfaa1c98eb
이정섭 <dark6ro@naver.com>
2023-11-03 11:14:20 +0900

    [S09P31B108-378] fix : 관리자 목소리 dto 수정

--- backend/src/main/java/com/doran/admin_voice/dto/req/AdminVoiceInsertDto.java
+++ backend/src/main/java/com/doran/admin_voice/dto/req/AdminVoiceInsertDto.java
@@ -2,6 +2,8 @@ package com.doran.admin_voice.dto.req;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import com.doran.admin_voice.type.Genders;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,5 +13,6 @@ import lombok.Setter;
 @AllArgsConstructor
 public class AdminVoiceInsertDto {
     private int contentId;
+    private Genders gender; // MALE or FEMALE
     private MultipartFile file;
 }
