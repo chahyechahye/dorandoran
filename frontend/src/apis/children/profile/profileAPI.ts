2292003119fbdc8d46a786ffcaf640f45d25f168
cje9717@naver.com <cje9717@naver.com>
2023-11-04 16:02:23 +0900

    [S09P31B108-431] feat : children profile API 추가

--- frontend/src/apis/children/profile/profileAPI.ts
+++ frontend/src/apis/children/profile/profileAPI.ts
@@ -4,7 +4,10 @@ import axios from "axios";
 
 const getChildrenCode = async (code: number) => {
   try {
-    const response = await axios.get(`/profile?code=${code}`);
+    const response = await axios.get(
+      `https://dorandoran.site/api/profile?code=${code}`
+    );
+
     return response.data;
   } catch {
     new Error("api 연동 오류 - getChildrenCode");
@@ -13,14 +16,21 @@ const getChildrenCode = async (code: number) => {
 
 const postChildrenLogin = async (ChildrenLoginData: ChildrenLoginProps) => {
   try {
-    const response = await axios.post(`/profile/login`, ChildrenLoginData);
+    const response = await axios.post(
+      `https://dorandoran.site/api/profile/login`,
+      ChildrenLoginData
+    );
+
+    const accessToken = response.headers.accesstoken;
+    localStorage.setItem("accesstoken", accessToken);
+
     return response.data;
   } catch {
     new Error("api 연동 오류 - postChildrenLogin");
   }
 };
 
-const postChildrenCharacter = async (animalId: number) => {
+const postChildrenCharacter = async (animalId: string) => {
   try {
     const response = await instance.post(`/profile/animal`, animalId);
     return response;
