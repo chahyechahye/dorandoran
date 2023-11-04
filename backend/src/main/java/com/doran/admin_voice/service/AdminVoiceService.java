9d2e76ae03c2c1233e9c48b16702539cf4c2b5d3
이정섭 <dark6ro@naver.com>
2023-11-04 15:20:15 +0900

    [S09P31B108-378] fix : 관리자 목소리 조회 기능 mapper로 수정

--- backend/src/main/java/com/doran/admin_voice/service/AdminVoiceService.java
+++ backend/src/main/java/com/doran/admin_voice/service/AdminVoiceService.java
@@ -31,26 +31,23 @@ public class AdminVoiceService {
     private final ContentService contentService;
     private final BucketMapper bucketMapper;
     private final BucketService bucketService;
-
     // content (한 줄)단위 관리자 목소리
-    public AdminVoiceResDto findAdminVoiceByContentId(Genders gender, int contentId) {
+    public AdminVoiceResDto findAdminVoiceByContentId(Genders gender, int contentId){
         return adminVoiceMapper.toResDto(
             adminVoiceRepository.findAdminVoiceByContentId(gender, contentId)
-                .orElseThrow(() -> new CustomException(ErrorCode.VOICE_NOT_FOUND)));
+            .orElseThrow(() -> new CustomException(ErrorCode.VOICE_NOT_FOUND)));
     }
-
     // book (동화책)단위 관리자 목소리 -> 뭔가 쓸일이 있을 거 같아서 일단 만듦
-    public List<AdminVoiceResDto> findAdminVoiceByBookId(Genders gender, int bookId) {
+    public List<AdminVoiceResDto> findAdminVoiceByBookId(Genders gender, int bookId){
         List<AdminVoice> result = adminVoiceRepository.findAdminVoiceByBookId(gender, bookId);
         return adminVoiceMapper.toResDtoList(result);
     }
-
-    public void insertAdminVoice(AdminVoiceInsertDto adminVoiceInsertDto) {
+    public void insertAdminVoice(AdminVoiceInsertDto adminVoiceInsertDto){
         Content content = contentService.getContentById(adminVoiceInsertDto.getContentId());
         String voiceUrl = bucketService.insertFile(
             bucketMapper.toInsertDto(adminVoiceInsertDto.getFile(), "admin_voice")
         );
-        AdminVoice adminVoice = adminVoiceMapper.dtoToEntity(adminVoiceInsertDto, voiceUrl, content);
+        AdminVoice adminVoice = adminVoiceMapper.dtoToEntity(adminVoiceInsertDto,voiceUrl,content);
         adminVoiceRepository.save(adminVoice);
     }
 
