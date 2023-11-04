e3e24bf81d7e4d6fd938488ddaba4cbcfe78dbd9
cje9717@naver.com <cje9717@naver.com>
2023-11-04 16:01:39 +0900

    [S09P31B108-431] feat : fairytale API 추가

--- frontend/src/apis/children/fairytale/Queries/useFariytaleList.ts
+++ frontend/src/apis/children/fairytale/Queries/useFariytaleList.ts
@@ -3,6 +3,7 @@ import { getFairytaleList } from "@/apis/children/fairytale/fairytaleAPI";
 
 const useFairytaleList = () => {
   const { data } = useQuery(["FairytaleList"], () => getFairytaleList());
+  console.log("완료1");
   return data;
 };
 
--- frontend/src/apis/children/fairytale/fairytaleAPI.ts
+++ frontend/src/apis/children/fairytale/fairytaleAPI.ts
@@ -2,9 +2,11 @@ import axios from "axios";
 
 const getFairytaleList = async () => {
   try {
-    const response = await axios.get(`/book`);
+    const response = await axios.get(`https://dorandoran.site/api/book`);
+    console.log("완료2");
     return response.data;
-  } catch {
+  } catch (error) {
+    console.error("API 연동 오류 - getFairytaleList:", error);
     new Error("api 연동 오류 - getFairytaleList");
   }
 };
