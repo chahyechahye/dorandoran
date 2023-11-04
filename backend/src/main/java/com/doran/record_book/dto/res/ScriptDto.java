f108c8733f176e8dd64279dbb8171f342096a231
한성현 <hsh5349@kakao.com>
2023-11-04 20:39:00 +0900

    [S09P31B108-360] feat : dto 생성

--- backend/src/main/java/com/doran/record_book/dto/res/ScriptDto.java
+++ backend/src/main/java/com/doran/record_book/dto/res/ScriptDto.java
@@ -2,11 +2,13 @@ package com.doran.record_book.dto.res;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ScriptDto {
     private String script;
 
-    private int scriptNUm;
+    private int scriptNum;
 }
