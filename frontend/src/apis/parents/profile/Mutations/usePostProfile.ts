7b0a7a1101589392952496c1520f1fac197150d7
Ssuhyeong <tngud124@gmail.com>
2023-11-04 15:23:49 +0900

    [S09P31B108-395] fix 프로필 api 수정

--- frontend/src/apis/parents/profile/Mutations/usePostProfile.ts
+++ frontend/src/apis/parents/profile/Mutations/usePostProfile.ts
@@ -1,11 +1,13 @@
 import { ProfileProps } from "@/types/parent/profileType";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { postProfile } from "@/apis/parents/profile/profileAPI";
 
 const usePostProfile = () => {
+  const queryCilent = useQueryClient();
   return useMutation((name: ProfileProps) => postProfile(name), {
     onSuccess: () => {
       console.log("usePostProfile Success");
+      queryCilent.invalidateQueries(["ProfileList"]);
     },
     onError: (err: Error) => {
       console.log(err);
