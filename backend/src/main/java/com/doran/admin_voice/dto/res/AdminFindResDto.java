5e340e9426140c54d5faf440a7e55b752dcee0c3
홍지민 <baker0929@naver.com>
2023-11-05 18:49:24 +0900

    [S09P31B108-340] feat : 목소리 생성 요청시 조회용 Dto 생성

---
+++ backend/src/main/java/com/doran/admin_voice/dto/res/AdminFindResDto.java
@@ -0,0 +1,19 @@
+package com.doran.admin_voice.dto.res;
+
+import java.util.List;
+
+import com.doran.admin_voice.entity.AdminVoice;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AdminFindResDto {
+    int bookId;
+    List<AdminVoiceResDto> adminVoiceList;
+}
