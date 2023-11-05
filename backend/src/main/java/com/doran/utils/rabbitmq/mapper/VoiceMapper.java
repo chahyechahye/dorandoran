cae1aca5269a682f75f9fc86d4fc06f032af9777
홍지민 <baker0929@naver.com>
2023-11-05 18:48:45 +0900

    [S09P31B108-340] feat : Voice큐를 위한 매퍼 생성

---
+++ backend/src/main/java/com/doran/utils/rabbitmq/mapper/VoiceMapper.java
@@ -0,0 +1,14 @@
+package com.doran.utils.rabbitmq.mapper;
+
+import java.util.List;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import com.doran.admin_voice.dto.res.AdminFindResDto;
+import com.doran.utils.rabbitmq.dto.req.VoiceReqMessage;
+
+@Mapper(componentModel = "spring")
+public interface VoiceMapper {
+    VoiceReqMessage toReqMessage(int userId, List<AdminFindResDto> list);
+}
