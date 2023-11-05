c1ea70ad5cc8960be858936f282ed49d7a7c9e9d
홍지민 <baker0929@naver.com>
2023-11-05 18:46:04 +0900

    [S09P31B108-340] feat : 가공 목소리 생성 요청 서비스 구현

--- backend/src/main/java/com/doran/utils/rabbitmq/service/VoicePubService.java
+++ backend/src/main/java/com/doran/utils/rabbitmq/service/VoicePubService.java
@@ -1,10 +1,15 @@
 package com.doran.utils.rabbitmq.service;
 
+import java.util.List;
+
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.doran.admin_voice.dto.res.AdminFindResDto;
+import com.doran.admin_voice.service.AdminVoiceService;
 import com.doran.utils.rabbitmq.dto.req.VoiceReqMessage;
+import com.doran.utils.rabbitmq.mapper.VoiceMapper;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,12 +19,16 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class VoicePubService {
     private final RabbitTemplate rabbitTemplate;
+    private final AdminVoiceService adminVoiceService;
+    private final VoiceMapper voiceMapper;
 
     @Value("${rabbitmq.queue.voice}")
     private String routingKey;
 
-    public void sendMessage(VoiceReqMessage voiceReqMessage)
-    {
+    public void sendMessage(int userId) {
+        List<AdminFindResDto> list = adminVoiceService.findAdminVoiceAndBook();
+        VoiceReqMessage voiceReqMessage = voiceMapper.toReqMessage(userId, list);
+
         log.info("가공 목소리 생성 요청 : " + voiceReqMessage.getUserId());
         rabbitTemplate.convertAndSend(routingKey, voiceReqMessage);
     }
