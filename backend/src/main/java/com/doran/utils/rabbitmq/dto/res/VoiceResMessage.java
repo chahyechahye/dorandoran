c1850729ecbb1f204b09846767c78b392068307e
홍지민 <baker0929@naver.com>
2023-11-05 18:44:13 +0900

    [S09P31B108-340] refactor : VoiceResMessage DTO 수정

--- backend/src/main/java/com/doran/utils/rabbitmq/dto/res/VoiceResMessage.java
+++ backend/src/main/java/com/doran/utils/rabbitmq/dto/res/VoiceResMessage.java
@@ -1,6 +1,9 @@
 package com.doran.utils.rabbitmq.dto.res;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import com.doran.processed_voice.dto.res.PVQueResDto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,5 +20,5 @@ import lombok.ToString;
 @ToString
 public class VoiceResMessage {
     private int userId;
-    private List<String> urlList;
+    private List<PVQueResDto> pvList;
 }
