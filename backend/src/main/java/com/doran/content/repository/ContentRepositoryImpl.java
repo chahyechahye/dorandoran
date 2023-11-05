1200eea90b614048b63ee04cd87c9dc779fd6ed5
한성현 <hsh5349@kakao.com>
2023-11-05 16:09:28 +0900

    [S09P31B108-363] feat : 컨텐츠 조회 쿼리 생성

--- backend/src/main/java/com/doran/content/repository/ContentRepositoryImpl.java
+++ backend/src/main/java/com/doran/content/repository/ContentRepositoryImpl.java
@@ -1,14 +1,15 @@
 package com.doran.content.repository;
 
+import static com.doran.content.entity.QContent.*;
+import static com.doran.processed_voice.entity.QProcessedVoice.*;
+
 import java.util.List;
 
 import com.doran.content.dto.res.ContentResDto;
+import com.doran.page.entity.Page;
 import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-
-import static com.doran.content.entity.QContent.content;
-import static com.doran.processed_voice.entity.QProcessedVoice.processedVoice;
 
 import lombok.RequiredArgsConstructor;
 
@@ -24,7 +25,7 @@ public class ContentRepositoryImpl implements ContentRepositoryCustom {
                 content.id.as("content_id"),
                 content.script.as("script"),
                 processedVoice.voiceUrl.as("voiceUrl")
-                ))
+            ))
             .from(content)
             .leftJoin(content.processedVoice, processedVoice)
             .on(processedVoice.user.id.eq(userId))
@@ -33,9 +34,19 @@ public class ContentRepositoryImpl implements ContentRepositoryCustom {
             .fetch();
     }
 
+    @Override
+    public List<String> findContentByPageList(List<Page> pageList) {
+        return jpaQueryFactory
+            .select(content.script)
+            .from(content)
+            .where(content.page.in(pageList))
+            .fetch();
+    }
+
     private static BooleanExpression pageIdEq(Integer id) {
         return id != null ? content.page.id.eq(id) : null;
     }
+
     private static BooleanExpression bookIdEq(Integer id) {
         return id != null ? content.page.book.id.eq(id) : null;
     }
