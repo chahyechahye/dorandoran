489cda72a978cd01e078e4a698771c9a0b74f541
홍지민 <baker0929@naver.com>
2023-11-05 18:50:15 +0900

    [S09P31B108-340] feat : 테스트 코드

---
+++ backend/src/test/java/com/doran/admin_voice/repository/AdminVoiceRepositoryCustomImplTest.java
@@ -0,0 +1,32 @@
+package com.doran.admin_voice.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.doran.admin_voice.dto.res.AdminFindResDto;
+import com.doran.admin_voice.dto.res.AdminVoiceResDto;
+
+@SpringBootTest
+public class AdminVoiceRepositoryCustomImplTest {
+    @Autowired
+    private AdminVoiceRepository adminVoiceRepository;
+
+    @Test
+    public void 관리자목소리_테스트() {
+        List<AdminFindResDto> list = adminVoiceRepository.findAdminVoiceAndBook();
+        if (list.size() == 0)
+            System.out.println("응애 아무것도 없어요");
+        for (AdminFindResDto a : list) {
+            System.out.println(a.getBookId() + "------------------------");
+            for (AdminVoiceResDto b : a.getAdminVoiceList())
+                System.out.print(b.getContentId() + " ");
+            System.out.println();
+        }
+
+    }
+}
