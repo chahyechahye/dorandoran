6d112e3c1d4e58530084ec9c7ae6522c36a2d1b1
이정섭 <dark6ro@naver.com>
2023-11-05 14:33:21 +0900

    [S09P31B108-378] fix : 편지 등록 기능 수정 및 안 읽은 편지 개수 반환 기능 추가

--- backend/src/main/java/com/doran/letter/service/LetterService.java
+++ backend/src/main/java/com/doran/letter/service/LetterService.java
@@ -5,8 +5,11 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.doran.child.entity.Child;
+import com.doran.child.service.ChildService;
 import com.doran.letter.dto.req.LetterInsertDto;
 import com.doran.letter.dto.res.LetterResDto;
+import com.doran.letter.dto.res.UnreadLetterResDto;
 import com.doran.letter.entity.Letter;
 import com.doran.letter.mapper.LetterMapper;
 import com.doran.letter.repository.LetterRepository;
@@ -15,6 +18,8 @@ import com.doran.parent.service.ParentService;
 import com.doran.profile.entity.Profile;
 import com.doran.profile.repository.ProfileRepository;
 import com.doran.profile.service.ProfileService;
+import com.doran.user.repository.UserRepository;
+import com.doran.user.service.UserService;
 import com.doran.utils.auth.Auth;
 import com.doran.utils.bucket.mapper.BucketMapper;
 import com.doran.utils.bucket.service.BucketService;
@@ -29,6 +34,7 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 @Slf4j
 public class LetterService{
+    private final UserRepository userRepository;
     private final ParentService parentService; // 부모 아이 판별을 위해 import
     private final ProfileService profileService;
     private final BucketMapper bucketMapper;
@@ -38,34 +44,39 @@ public class LetterService{
     // 편지 조회
     public LetterResDto getLetter(int userId){
         List<Letter> unreadLetterList = letterRepository.findAllUnreadLetter(userId);
-        if(unreadLetterList.size() == 0) throw new CustomException(ErrorCode.LETTER_NOT_FOUND);
+        if(unreadLetterList.isEmpty()) throw new CustomException(ErrorCode.LETTER_NOT_FOUND);
         Letter letter = unreadLetterList.get(0);
         letter.setModifiedDate(LocalDateTime.now());
         letterRepository.save(letter);
         LetterResDto letterResDto = letterMapper.letterToResDto(letter,unreadLetterList.size()-1);
         return letterResDto;
     }
+    public UnreadLetterResDto getUnreadLetterCount(int userId){
+        List<Letter> unreadLetterList = letterRepository.findAllUnreadLetter(userId);
+        return letterMapper.letterToUnreadResDto(unreadLetterList.size());
+    }
     // 편지 등록
     public Letter insertLetter(LetterInsertDto letterInsertDto){
+        log.info("편지 등록");
         UserInfo userInfo = Auth.getInfo();
         Parent parent = null;
         Profile profile = null;
-        int receiverId=0, senderId=0;
-        profile = profileService.findProfileById(letterInsertDto.getProfileId());
+        int receiverId=0;
+        int senderId = userInfo.getUserId();
         if(parentService.checkParent(userInfo.getUserRole().getRole())){
             // 보내는 사람 프로필(아이), 받는 사람 부모일 때
-            parent = parentService.findParentByProfileId(letterInsertDto.getProfileId());
-            receiverId = parent.getUser().getId();
-            senderId = profile.getChild().getUser().getId();
+            profile = profileService.findProfileById(userInfo.getSelectProfileId());
+            parent = parentService.findParentByProfileId(profile.getId());
+            receiverId = parentService.getParentUserId(userInfo.getUserId(),userInfo.getUserRole().getRole());
         }else{
             // 보내는 사람 부모, 받는 사람 프로필(아이)일 때 -> 이때 부모는 null 상태로 저장
+            profile = profileService.findProfileById(letterInsertDto.getProfileId());
             parent = parentService.findParentByUserId(userInfo.getUserId());
-            receiverId = profile.getChild().getUser().getId();
-            senderId = parent.getUser().getId();
+            receiverId = userRepository.findUserByProfileId(letterInsertDto.getProfileId())
+                .orElseThrow(()->new CustomException(ErrorCode.USER_NOT_FOUND)).getId();
         }
         String contentUrl = bucketService.insertFile(bucketMapper.toInsertDto(letterInsertDto.getContent(), "letter"));
         Letter letter = letterMapper.insertLettertoLetter(letterInsertDto,parent,profile,contentUrl,receiverId,senderId);
-        // 편지 저장 시 파일 이름
         return letterRepository.save(letter);
     }
 }
