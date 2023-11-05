420439a11d7e42fcd7eee25618a4504282ab4f26
한성현 <hsh5349@kakao.com>
2023-11-05 16:08:52 +0900

    [S09P31B108-363] feat : 매퍼 생성

--- backend/src/main/java/com/doran/record_book/mapper/RecordBookMapper.java
+++ backend/src/main/java/com/doran/record_book/mapper/RecordBookMapper.java
@@ -7,6 +7,7 @@ import org.mapstruct.Mapper;
 import com.doran.record_book.dto.res.BookDto;
 import com.doran.record_book.dto.res.RecordBookResDto;
 import com.doran.record_book.dto.res.ScriptDto;
+import com.doran.record_book.entity.RecordBook;
 
 @Mapper(componentModel = "spring")
 public interface RecordBookMapper {
@@ -14,4 +15,6 @@ public interface RecordBookMapper {
     BookDto toBookDto(String title, List<ScriptDto> scriptList);
 
     RecordBookResDto toResDto(List<Long> totalScriptList, List<BookDto> bookList);
+
+    RecordBook toRecordBook(String title, String script, int scriptNum);
 }
