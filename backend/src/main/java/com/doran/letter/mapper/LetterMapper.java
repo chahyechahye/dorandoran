f51f0cf187831c949739b00eb1d6d8f57180bd9d
이정섭 <dark6ro@naver.com>
2023-11-06 13:18:24 +0900

    [S09P31B108-377] fix : letterMapper 수정

--- backend/src/main/java/com/doran/letter/mapper/LetterMapper.java
+++ backend/src/main/java/com/doran/letter/mapper/LetterMapper.java
@@ -1,10 +1,13 @@
 package com.doran.letter.mapper;
 
+import java.util.List;
+
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 import com.doran.letter.dto.req.LetterInsertDto;
 import com.doran.letter.dto.res.LetterResDto;
+import com.doran.letter.dto.res.LetterResDtoList;
 import com.doran.letter.dto.res.UnreadLetterResDto;
 import com.doran.letter.entity.Letter;
 import com.doran.parent.entity.Parent;
@@ -20,7 +23,10 @@ public interface LetterMapper {
     UnreadLetterResDto letterToUnreadResDto(Integer size);
 
 
-    LetterResDto letterToResDto(Letter letter, int unreadCount);
+    @Mapping(source="id", target="letterId")
+    LetterResDto letterToResDto(Letter letter);
+    List<LetterResDto> letterListToResDtoList(List<Letter> letterList);
+    LetterResDtoList letterListToResDtoList(List<LetterResDto> letterResDtoList, Integer size);
 
 
 }
