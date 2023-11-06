d40e4d2d8a7fab67202f78888ff2f781170ab3de
이정섭 <dark6ro@naver.com>
2023-11-06 13:17:46 +0900

    [S09P31B108-377] feat : 읽은 편지 갱신 dto 추가

---
+++ backend/src/main/java/com/doran/letter/dto/req/LetterReadDto.java
@@ -0,0 +1,14 @@
+package com.doran.letter.dto.req;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LetterReadDto {
+    Integer letterId;
+}
