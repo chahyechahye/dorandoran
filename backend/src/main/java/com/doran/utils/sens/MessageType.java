7ad8ffbeca4ce4eca7131a263da83afa28247fb3
HongJiMin <baker0929@naver.com>
2023-11-06 14:01:24 +0900

    [S09P31B108-479] refactor : 메시지 Enum에 String 추가

--- backend/src/main/java/com/doran/utils/sens/MessageType.java
+++ backend/src/main/java/com/doran/utils/sens/MessageType.java
@@ -9,7 +9,9 @@ import lombok.Setter;
 @Getter
 @AllArgsConstructor
 public enum MessageType {
-    INVITE,
-    MODEL,
-    VOICE
+    INVITE("도란도란 초대 코드 : ["),
+    MODEL("목소리 모델 생성이 완료되었습니다."),
+    VOICE("동화 등록이 완료되었습니다.");
+
+    private final String msg;
 }
