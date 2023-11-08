d1bad5ee6446ab9e0e1ff304b442d2e45b7e93b3
황준혁 <hyug0819@gmail.com>
2023-11-08 15:34:01 +0900

    [S09P31B108-000] fix : try except 추가

--- fastapi/app/GoogleBucket.py
+++ fastapi/app/GoogleBucket.py
@@ -4,48 +4,54 @@ os.environ ["GOOGLE_APPLICATION_CREDENTIALS"] = "rd-ssafy-project-ebd0eea46d3e.j
 
 from google.cloud import storage
 
+from customLog import LogInfo
+from customLog import LogError
+
 client = storage.Client()
 
 bucket_name = 'ssafy-last-project'
 
 def Download(bookId, gender, voiceUrl):
+    try:
+        data = {
+            "directory" : "",
+            "file_name" : "",
+            "save_location" : "",
+            "content_id" : ""
+        }
 
-    data = {
-        "directory" : "",
-        "file_name" : "",
-        "save_location" : "",
-        "content_id" : ""
-    }
+        fileName = voiceUrl.split("/")[-1]
+        LogInfo(fileName)
+        
+        directory = os.path.join("/", "app", "data", f"book_{str(bookId)}_{gender}")
+        os.makedirs(directory, exist_ok=True)
+        save_location = os.path.join(directory, fileName+".wav")
 
-    fileName = voiceUrl.split("/")[-1]
-    print(fileName)
-    
-    directory = os.path.join("/", "app", "data", f"book_{str(bookId)}_{gender}")
-    os.makedirs(directory, exist_ok=True)
-    save_location = os.path.join(directory, fileName+".wav")
+        if os.path.exists(save_location):
+            LogInfo("aleady download")
+            return
 
-    if os.path.exists(save_location):
-        print("aleady download")
-        return
-
-    bucket = client.bucket(bucket_name)
-    blob = bucket.blob(fileName)
-    blob.download_to_filename(save_location)
-    data['directory'] = directory
-    data['file_name'] = fileName
-    data['save_location'] = save_location
-    return data
+        bucket = client.bucket(bucket_name)
+        blob = bucket.blob(fileName)
+        blob.download_to_filename(save_location)
+        data['directory'] = directory
+        data['file_name'] = fileName
+        data['save_location'] = save_location
+        return data
+    except Exception as e:
+        LogError(e)
+        LogError("Download Fail")
 
 def DownloadRaw(userId, gender, voiceUrl):
     fileName = voiceUrl.split("/")[-1]
-    print(fileName)
+    LogInfo(fileName)
     
     directory = os.path.join("/", "app", "data", f"user_{str(userId)}_{gender}")
     os.makedirs(directory, exist_ok=True)
     save_location = os.path.join(directory, fileName+".wav")
 
     if os.path.exists(save_location):
-        print("aleady download")
+        LogInfo("aleady download")
         return
 
     bucket = client.bucket(bucket_name)
@@ -54,14 +60,17 @@ def DownloadRaw(userId, gender, voiceUrl):
     return directory
 
 def Upload(userId, fileName):
+    try:
+        directory = os.path.join("/", "app", "opt", str(userId), fileName)
 
-    directory = os.path.join("/", "app", "opt", str(userId), fileName)
+        destination_file_name = str(uuid.uuid3(uuid.NAMESPACE_URL, directory))
 
-    destination_file_name = str(uuid.uuid3(uuid.NAMESPACE_URL, directory))
-
-    bucket = client.bucket(bucket_name)
-    blob = bucket.blob(destination_file_name)
-    generation_match_precondition = 0
-    blob.upload_from_filename(directory, if_generation_match=generation_match_precondition)
-    print(destination_file_name)
-    return destination_file_name
+        bucket = client.bucket(bucket_name)
+        blob = bucket.blob(destination_file_name)
+        generation_match_precondition = 0
+        blob.upload_from_filename(directory, if_generation_match=generation_match_precondition)
+        LogInfo(destination_file_name)
+        return destination_file_name
+    except Exception as e:
+        LogError(e)
+        LogError("Upload Fail")
