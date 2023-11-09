34cf2f1d33fbc4eda8873f5872817b19507e8354
황준혁 <hyug0819@gmail.com>
2023-11-09 14:17:04 +0900

    [S09P31B108-000] refactor : gender 추가 & Dto 제거

--- backend/src/main/java/com/doran/admin_voice/mapper/AdminVoiceMapper.java
+++ backend/src/main/java/com/doran/admin_voice/mapper/AdminVoiceMapper.java
@@ -5,16 +5,18 @@ import java.util.List;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-import com.doran.admin_voice.dto.req.AdminVoiceInsertDto;
 import com.doran.admin_voice.dto.res.AdminVoiceResDto;
 import com.doran.admin_voice.entity.AdminVoice;
 import com.doran.content.entity.Content;
+import com.doran.utils.common.Genders;
 
 @Mapper(componentModel = "spring")
 public interface AdminVoiceMapper {
-    @Mapping(source="content.id", target="contentId")
+    @Mapping(source = "content.id", target = "contentId")
     AdminVoiceResDto toResDto(AdminVoice adminVoice);
+
     List<AdminVoiceResDto> toResDtoList(List<AdminVoice> adminVoiceList);
-    @Mapping(target="id", ignore = true)
-    AdminVoice dtoToEntity(AdminVoiceInsertDto adminVoiceInsertDto, String voiceUrl, Content content);
+
+    @Mapping(target = "id", ignore = true)
+    AdminVoice dtoToEntity(String voiceUrl, Genders genders, Content content);
 }
--- backend/src/main/java/com/doran/admin_voice/service/AdminVoiceService.java
+++ backend/src/main/java/com/doran/admin_voice/service/AdminVoiceService.java
@@ -1,6 +1,5 @@
 package com.doran.admin_voice.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -11,11 +10,11 @@ import com.doran.admin_voice.dto.res.AdminVoiceResDto;
 import com.doran.admin_voice.entity.AdminVoice;
 import com.doran.admin_voice.mapper.AdminVoiceMapper;
 import com.doran.admin_voice.repository.AdminVoiceRepository;
-import com.doran.utils.common.Genders;
 import com.doran.content.entity.Content;
 import com.doran.content.service.ContentService;
 import com.doran.utils.bucket.mapper.BucketMapper;
 import com.doran.utils.bucket.service.BucketService;
+import com.doran.utils.common.Genders;
 import com.doran.utils.exception.dto.CustomException;
 import com.doran.utils.exception.dto.ErrorCode;
 
@@ -33,23 +32,25 @@ public class AdminVoiceService {
     private final BucketService bucketService;
 
     // content (한 줄)단위 관리자 목소리
-    public AdminVoiceResDto findAdminVoiceByContentId(Genders gender, int contentId){
+    public AdminVoiceResDto findAdminVoiceByContentId(Genders gender, int contentId) {
         return adminVoiceMapper.toResDto(
             adminVoiceRepository.findAdminVoiceByContentId(gender, contentId)
-            .orElseThrow(() -> new CustomException(ErrorCode.VOICE_NOT_FOUND)));
+                                .orElseThrow(() -> new CustomException(ErrorCode.VOICE_NOT_FOUND)));
     }
 
     // book (동화책)단위 관리자 목소리 -> 뭔가 쓸일이 있을 거 같아서 일단 만듦
-    public List<AdminVoiceResDto> findAdminVoiceByBookId(Genders gender, int bookId){
+    public List<AdminVoiceResDto> findAdminVoiceByBookId(Genders gender, int bookId) {
         List<AdminVoice> result = adminVoiceRepository.findAdminVoiceByBookId(gender, bookId);
         return adminVoiceMapper.toResDtoList(result);
     }
-    public void insertAdminVoice(AdminVoiceInsertDto adminVoiceInsertDto){
+
+    public void insertAdminVoice(AdminVoiceInsertDto adminVoiceInsertDto) {
         Content content = contentService.getContentById(adminVoiceInsertDto.getContentId());
         String voiceUrl = bucketService.insertFile(
             bucketMapper.toInsertDto(adminVoiceInsertDto.getFile(), "admin_voice")
         );
-        AdminVoice adminVoice = adminVoiceMapper.dtoToEntity(adminVoiceInsertDto,voiceUrl,content);
+        AdminVoice adminVoice = adminVoiceMapper.dtoToEntity(voiceUrl,
+            adminVoiceInsertDto.getGender(), content);
         adminVoiceRepository.save(adminVoice);
     }
 
