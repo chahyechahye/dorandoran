6f92e5d4dc35be753fd1701459f870e6a24b4154
한성현 <hsh5349@kakao.com>
2023-11-12 19:56:51 +0900

    [S09P31B108-466] feat : 중간 저장 api 생성

--- backend/src/main/java/com/doran/record_book/controller/RecordBookController.java
+++ backend/src/main/java/com/doran/record_book/controller/RecordBookController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,8 +15,14 @@ import com.doran.book.service.BookService;
 import com.doran.content.service.ContentService;
 import com.doran.page.entity.Page;
 import com.doran.page.service.PageService;
+import com.doran.record_book.dto.req.SavePostDto;
 import com.doran.record_book.dto.res.RecordBookResDto;
+import com.doran.record_book.entity.RecordBook;
 import com.doran.record_book.service.RecordBookService;
+import com.doran.redis.script.mapper.ScriptMapper;
+import com.doran.redis.script.service.ScriptService;
+import com.doran.utils.auth.Auth;
+import com.doran.utils.common.UserInfo;
 import com.doran.utils.response.CommonResponseEntity;
 import com.doran.utils.response.SuccessCode;
 
@@ -32,6 +39,8 @@ public class RecordBookController {
     private final BookService bookService;
     private final PageService pageService;
     private final ContentService contentService;
+    private final ScriptService scriptService;
+    private final ScriptMapper scriptMapper;
 
     //스크립트 전체 조회
     @GetMapping()
@@ -55,5 +64,17 @@ public class RecordBookController {
 
         recordBookService.regist(contentByPageList, findBook.getTitle());
         return CommonResponseEntity.getResponseEntity(SuccessCode.OK, "등록 완료");
+    }
+
+    //중간저장
+    @PostMapping("/save")
+    public ResponseEntity saveScript(@RequestBody SavePostDto dto) {
+        UserInfo info = Auth.getInfo();
+
+        RecordBook script = recordBookService.findScript(dto.getScript(), dto.getScriptNum());
+
+        scriptService.save(scriptMapper.toScript(info.getUserId(), script.getScript(), script.getScriptNum()));
+
+        return CommonResponseEntity.getResponseEntity(SuccessCode.OK, "중간 저장 완료");
     }
 }
