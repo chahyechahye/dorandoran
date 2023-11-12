4067d8855f307cd659e983d63b9813db21a27599
한성현 <hsh5349@kakao.com>
2023-11-12 23:44:34 +0900

    [S09P31B108-465] feat : 리스트 매퍼 생성

--- backend/src/main/java/com/doran/raw_voice/mapper/RawVoiceMapper.java
+++ backend/src/main/java/com/doran/raw_voice/mapper/RawVoiceMapper.java
@@ -1,28 +1,34 @@
 package com.doran.raw_voice.mapper;
 
-import com.doran.raw_voice.dto.req.RawVoiceInsertDto;
+import java.util.List;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
 import com.doran.raw_voice.dto.res.RawVoiceListDto;
 import com.doran.raw_voice.dto.res.RawVoiceResDto;
 import com.doran.raw_voice.entity.RawVoice;
 import com.doran.user.entity.User;
 import com.doran.utils.common.Genders;
 
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-
-import java.util.List;
-
 @Mapper(componentModel = "spring")
 public interface RawVoiceMapper {
-//    RawVoice voiceInsertToRawVoice(RawVoiceInsertDto rawVoiceInsertDto, String voiceUrl);
-    @Mapping(source="id", target="rvId")
-    @Mapping(source="voiceGender", target="gender")
+    //    RawVoice voiceInsertToRawVoice(RawVoiceInsertDto rawVoiceInsertDto, String voiceUrl);
+    @Mapping(source = "id", target = "rvId")
+    @Mapping(source = "voiceGender", target = "gender")
     RawVoiceResDto rawVoiceToResDto(RawVoice rawVoice);
+
     RawVoiceListDto listToResListDto(List<RawVoiceResDto> rawVoiceResDtoList, int size);
 
     List<RawVoiceResDto> toDtoList(List<RawVoice> rawVoiceList);
 
-    @Mapping(target="id", ignore = true)
-    @Mapping(source="gender", target = "voiceGender")
-    RawVoice voiceInsertToRawVoice(User user,String voiceUrl, Genders gender);
+    @Mapping(target = "id", ignore = true)
+    @Mapping(source = "gender", target = "voiceGender")
+    RawVoice voiceInsertToRawVoice(User user, String voiceUrl, Genders gender);
+
+    default List<String> toList(List<RawVoice> list) {
+        return list.stream()
+            .map(RawVoice::getVoiceUrl)
+            .toList();
+    }
 }
