502a262d6b703b32333ce90ea43bb5be194c653d
황준혁 <hyug0819@gmail.com>
2023-11-14 19:51:46 +0900

    [S09P31B108-000] fix : log 추가

--- fastapi/app/Model.py
+++ fastapi/app/Model.py
@@ -42,8 +42,9 @@ def Model(data):
             gender = rawVoice['gender']
             LogInfo(f"gender : {gender}")
             download_path = DownloadRaw(userId=userId, gender=userGender, voiceUrl=voiceUrl)
-            
+
         directory = JoinWav(directory=download_path)
+        LogInfo(f"join 결과 : {directory}")
         # 학습
         trainStartAll(user=userId, gender=userGender, trainPath=directory)
         res = ModelReq(
