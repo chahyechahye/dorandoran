29df0dff5c728790c0f5e89220f10549f6857c51
Ssuhyeong <tngud124@gmail.com>
2023-11-14 17:19:05 +0900

    [S09P31B108-651] fix : 배경음악 반복재생 로직 작업

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,7 +11,15 @@ import { useRecoilState } from "recoil";
 function App() {
   const queryClient = new QueryClient();
   const [isPlaying, setIsPlaying] = useRecoilState(MainSoundState);
-  const [play, { stop }] = useSound(mainSound, { volume: 0.2 });
+  const [play, { stop }] = useSound(mainSound, {
+    volume: 0.2,
+    onend: () => {
+      // 재생이 끝나면 다시 재생
+      if (isPlaying) {
+        play();
+      }
+    },
+  });
 
   useEffect(() => {
     // Subsequent playback when isPlaying changes
