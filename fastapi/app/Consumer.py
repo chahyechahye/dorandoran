abc87fd0f32bb452f2d93fcfa7b2bdc0aac50f7f
황준혁 <hyug0819@gmail.com>
2023-11-15 10:42:01 +0900

    [S09P31B108-000] refactor : 메세지 큐 변경

--- fastapi/app/Consumer.py
+++ fastapi/app/Consumer.py
@@ -17,21 +17,51 @@ def selectQueue(queueName):
     elif queueName == voice_sub:
         return voice_pub
 
-async def on_message_callback(ch, method, properties, body):
-    # 메시지를 처리하는 비동기 코드 작성
-    try:
-        queueName = method.routing_key
-        LogInfo(f"RoutingKey : {queueName}")
-        LogInfo(f"Received message : {body}")
-        if queueName == model_sub:
-            res = Model(body)
-        elif queueName == voice_sub:
-            res = Voice(body)
-        LogInfo(f"RESULT BODY : {res}")
-        ch.basic_publish(exchange="", routing_key=selectQueue(queueName), body=res)
-        ch.basic_ack(delivery_tag=method.delivery_tag)
-    except Exception as e:
-        LogError(e)
+# async def on_message_callback(ch, method, properties, body):
+#     # 메시지를 처리하는 비동기 코드 작성
+#     try:
+#         queueName = method.routing_key
+#         LogInfo(f"RoutingKey : {queueName}")
+#         LogInfo(f"Received message : {body}")
+#         if queueName == model_sub:
+#             res = Model(body)
+#         elif queueName == voice_sub:
+#             res = Voice(body)
+#         LogInfo(f"RESULT BODY : {res}")
+#         ch.basic_publish(exchange="", routing_key=selectQueue(queueName), body=res)
+#         ch.basic_ack(delivery_tag=method.delivery_tag)
+#     except Exception as e:
+#         LogError(e)
+
+async def on_message_callback(message: aio_pika.IncomingMessage):
+     async with message.process():
+        try:
+            queue_name = message.routing_key
+            body = message.body.decode()  # 메시지 바이트를 문자열로 디코딩
+
+            LogInfo(f"RoutingKey : {queue_name}")
+            LogInfo(f"Received message : {body}")
+
+            if queue_name == model_sub:
+                res = Model(body)
+            elif queue_name == voice_sub:
+                res = Voice(body)
+
+            LogInfo(f"RESULT BODY : {res}")
+
+            # 응답을 보내는 부분
+            channel = await message.channel()
+            exchange_name = ""  # 적절한 익스체인지 이름으로 변경
+            routing_key = selectQueue(queue_name)
+            await channel.default_exchange.publish(
+                aio_pika.Message(body=str(res).encode()),
+                routing_key=routing_key,
+                exchange_name=exchange_name
+            )
+
+        except Exception as e:
+            LogError(e)
+
 
 async def on_message(queue_name):
     # credentials = pika.PlainCredentials(username="username", password="password")
