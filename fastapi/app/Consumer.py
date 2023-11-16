ebff62759927dd7a17184f0dc4b4e43a3c57ad76
황준혁 <hyug0819@gmail.com>
2023-11-16 17:31:49 +0900

    [S09P31B108-000] refactor : a

--- fastapi/app/Consumer.py
+++ fastapi/app/Consumer.py
@@ -51,7 +51,7 @@ async def on_message_callback(message: aio_pika.IncomingMessage):
 
         except Exception as e:
             LogError(e)
-            message.reject(requeue=True)
+            await message.reject(requeue=True)
 
 rabbitmq_server_url = 'http://dorandoran.site:15672'
 rabbitmq_credentials = ('username', 'password')
